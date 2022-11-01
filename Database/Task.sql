ebba2566f73d1b023bb3c90a936dbbf93a966bb6
OBoyarov <80715535+OBoyarov@users.noreply.github.com>
2022-11-01 23:39:40 +0400

    Update Task.sql

--- Database/Task.sql
+++ Database/Task.sql
@@ -10,24 +10,3 @@ ON
   d.user_id = u.user_id
 WHERE
   u.name = N'Иван Иванов';
-  
-  
-  
-
-
---Before using query above
---Table users:
---INSERT INTO users (user_id, name) VALUES (1234567, 'Иван Иванов');
---INSERT INTO users (user_id, name) VALUES (1234568, 'Петр Петров');
---INSERT INTO users (user_id, name) VALUES (1234569, 'Семен Сидоров');
---INSERT INTO users (user_id, name) VALUES (1234570, 'Алексей Алексеев');
---INSERT INTO users (user_id, name) VALUES (1234571, 'Сергей Сергеев');
-
---Table domains:
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234567, 'ivanivanov.com');
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234567, 'ivanovivan.ru');
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234567, 'vanyavanya.org');
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234568, 'site.com');
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234569, 'site.ru');
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234570, 'domain.com');
---INSERT INTO u1614732_perl.domains (user_id, dname) VALUES (1234571, 'subdomain.net');
